2bd766b819931b64570dc87ff6e17b3b68c8f8e4
DevShiba <migerushiba@gmail.com>
2023-10-04 00:44:22 -0300

    Completing the Functional Part of the Application

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,27 +1,28 @@
-import React from "react";
+import { useGlobalContext } from "./context";
 
 const SearchForm = () => {
+  const { setSearchTerm } = useGlobalContext();
   const handleSubmit = (e) => {
     e.preventDefault();
-    const searchValue =  e.target.elements.search.value
-    if(!searchValue) return
+    const searchValue = e.target.elements.search.value;
+    if (!searchValue) return;
+    setSearchTerm(searchValue);
   };
   return (
     <section>
-      <h1 className="title">Unsplash Images</h1>
-      <form action="search-form" onSubmit={handleSubmit}>
+      <h1 className="title">unsplash images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
         <input
           type="text"
-          className="form-input search"
+          className="form-input search-input"
           name="search"
           placeholder="cat"
         />
         <button type="submit" className="btn">
-          Search
+          search
         </button>
       </form>
     </section>
   );
 };
-
 export default SearchForm;
--- src/context.jsx
+++ src/context.jsx
@@ -1,19 +1,34 @@
-import {useState, useContext, createContext, useEffect} from 'react'
+import { createContext, useContext, useState, useEffect } from "react";
 
 const AppContext = createContext();
 
-export const AppProvider = ({children})=>{
-    const [isDarkTheme, setIsDarkTheme] = useState(false)
+const getInitialDarkMode = () => {
+  const prefersDarkMode = window.matchMedia(
+    "(prefers-color-scheme:dark)"
+  ).matches;
+  const storedDarkMode = localStorage.getItem("darkTheme") === "true";
 
-    const toggleDarkTheme = ()=>{
-        const newDarkTheme = !isDarkTheme
-        setIsDarkTheme(newDarkTheme)
-        const body = document.querySelector('body');
-        body.classList.toggle('dark-theme', newDarkTheme)
-    }
-    return <AppContext.Provider value={{isDarkTheme, toggleDarkTheme}}>
-        {children}
+  return storedDarkMode || prefersDarkMode;
+};
+export const AppProvider = ({ children }) => {
+  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode());
+  const [searchTerm, setSearchTerm] = useState("cat");
+  const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+    localStorage.setItem("darkTheme", newDarkTheme);
+  };
+
+  useEffect(() => {
+    document.body.classList.toggle("dark-theme", isDarkTheme);
+  }, [isDarkTheme]);
+  return (
+    <AppContext.Provider
+      value={{ isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm }}
+    >
+      {children}
     </AppContext.Provider>
-}
+  );
+};
 
-export const useGlobalContext = ()=> useContext(AppContext)
+export const useGlobalContext = () => useContext(AppContext);
